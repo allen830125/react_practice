18c80f3a285b926eb22095336743654941b22dad
pohao <pohao.js.tw@tripleonetech.net>
2019-08-15 11:51:46 +0800

    fix react app 1. add lazy loading

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router'
 import store from './store';
-import ShoppingCart from './components/ShoppingCart';
-import TodoList from './components/TodoList';
+// import ShoppingCart from './components/ShoppingCart';
+// import TodoList from './components/TodoList';
 
-// const ShoppingCart = (location, cb) => {
-//     require.ensure(
-//         [],
-//         require => {
-//             cb(null, require('./components/ShoppingCart').default)
-//         },
-//         'shoppingCart'
-//     )
-// };
+const ShoppingCart = (location, cb) => {
+    import('./components/ShoppingCart')
+    .then(module => {
+        cb(null, module.default)
+    });
+};
+
+const TodoList = (location, cb) => {
+    import('./components/TodoList')
+    .then(module => {
+        cb(null, module.default)
+    });
+};
 
 // const TodoList = (location, cb) => {
 //     require.ensure(
@@ -30,8 +34,8 @@ class App extends Component {
         const getRoute = (store) => {
             return (
                 <Route path="/">
-                    <Route path="shoppingCart" component={ShoppingCart}></Route>
-                    <Route path="todoList" component={TodoList}></Route>
+                    <Route path="shoppingCart" getComponent={ShoppingCart}></Route>
+                    <Route path="todoList" getComponent={TodoList}></Route>
                 </Route>
             );
         }
